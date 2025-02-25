a202edb604ee58e932c6b623064cd97ac9420ae6
“Fahad” <Fahad.abdullah1280@gmail.com>
2025-02-25 11:49:20 +0600

    Create a test model, seed and test route

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,10 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Test;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Database\Factories\UserFactory;
+use Database\Factories\TestFactory;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,5 +17,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
          User::factory(10)->create();
+         Test::factory(20)->create();
     }
 }
